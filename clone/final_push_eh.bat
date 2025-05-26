e5245d82ed0f024247dafdf0d1c5140210839041
Eddy HUART <146103724+EddyHUART@users.noreply.github.com>
2025-05-26 15:40:17 -0400

    Add files via upload

---
+++ clone/final_push_eh.bat
@@ -0,0 +1,20 @@
+@echo off
+
+SET gitHubRepository=Session-4
+SET gitHubUsername=EddyHUART
+
+:: Créer le message de commit avec la date et l'heure
+SET commitMessage="Mise à jour"
+
+   :: Ajouter les fichiers modifiés
+    git add .
+
+    :: Effectuer le commit avec un message précis
+    git commit -m "Update"
+
+    :: Pousser les modifications vers GitHub
+    git push origin main
+	
+	echo.
+    echo  Commit et push effectues avec succes.
+pause
